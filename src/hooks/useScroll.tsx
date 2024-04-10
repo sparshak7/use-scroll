38f30a4e364233cd2222dc1c289a4b69f8864de6
sparshak7 <riley.onduty@gmail.com>
2024-04-10 17:35:09 +0530

    Initial commit

---
+++ src/hooks/useScroll.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { useEffect } from "react";
+
+type ScrollProps = {
+  breakpoint: number;
+};
+
+const useScroll = () => {
+  const scrollToTop = ({ breakpoint = 250 }: ScrollProps) => {
+    useEffect(() => {
+      if (window.scrollY > breakpoint) {
+        window.scrollTo(0, 0);
+      }
+    }, [breakpoint]);
+  };
+
+  return {scrollToTop}
+};
+
+export {useScroll};
